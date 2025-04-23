dea8557426016f3ec5fe8c4b8fb77aecd4112f61
vs <valeriisklyarov@gmail.com>
2025-04-23 13:54:35 +0300

    refactor(styles): remove unused CSS and SCSS files

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,23 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Spy.House</title>
-
-    <!-- Fonts -->
-    <!-- <link rel="preconnect" href="https://fonts.bunny.net">  
-    <link href="https://fonts.bunny.net/css?family=Montserrat:100,200,300,400,500,600,700,800,900" rel="stylesheet" /> -->
-
-    <!-- Styles / Scripts -->
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-    @vite([ 'resources/js/app.js', 'resources/scss/app.scss'])
-    @else
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    @endif
-</head>
+@section('content')
 
 <body class=" text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
     <div class="navigation-bg"></div>
@@ -322,9 +305,7 @@
     </div>
 
 
-    @if (Route::has('login'))
-    <div class="h-14.5 hidden lg:block"></div>
-    @endif
-</body>
+    <!-- @if (Route::has('login'))
+    @endif -->
 
-</html>
+    @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,31 +8,19 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/scss/app.scss'])
+    @vite([ 'resources/js/app.js', 'resources/scss/app.scss'])
 </head>
 
 <body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-        @include('layouts.navigation')
 
-        <!-- Page Heading -->
-        @isset($header)
-        <header class="bg-white dark:bg-gray-800 shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
-            </div>
-        </header>
-        @endisset
 
-        <!-- Page Content -->
-        <main>
-            {{ $slot }}
-        </main>
+    <!-- Page Content -->
+    <main>
+        @yield('content')
+    </main>
     </div>
 </body>
 
