2d249c99748e22b6ca23d05e0ef22d05eac8af5c
vs <valeriisklyarov@gmail.com>
2025-04-30 19:05:28 +0300

    refactor(services): clean up sorting functionality and remove comments

--- resources/js/services.js
+++ resources/js/services.js
@@ -1,9 +1,4 @@
-// resources/js/services.js
 document.addEventListener("DOMContentLoaded", function () {
-    // Filter toggle on mobile
-    console.log("Filter toggle on mobile");
-
-    // Sort functionality
     const sortContainer = $("#sort-by");
     if (sortContainer.length) {
         const sortSelect = $(".base-select__dropdown", sortContainer);
@@ -12,7 +7,6 @@ document.addEventListener("DOMContentLoaded", function () {
         const sortValue = $("[data-value]", sortContainer);
         const sortOrderValue = $("[data-order]", sortContainer);
 
-        // Toggle dropdown on trigger click
         sortTrigger.on("click", function (e) {
             e.stopPropagation();
             sortSelect.show();
@@ -21,26 +15,15 @@ document.addEventListener("DOMContentLoaded", function () {
         sortOptions.each(function () {
             $(this).on("click", function (e) {
                 e.stopPropagation();
-                // Remove selected class from all options
                 sortOptions.removeClass("is-selected");
-                // Add selected class to clicked option
                 $(this).addClass("is-selected");
-                // Update trigger text
-                // sortValue.text($(this).text());
                 sortValue.data("value", $(this).data("value"));
                 sortOrderValue.data("order", $(this).data("order"));
-                // Hide dropdown
                 sortSelect.hide();
-
-                // Get sort parameters
-                // const sortText = $(this).text();
 
                 let sortBy = sortValue.data("value");
                 let sortOrder = sortOrderValue.data("order");
 
-                // console.log(sortText);
-
-                // Update URL with sort parameters
                 const url = new URL(window.location.href);
                 if (sortBy && sortOrder) {
                     url.searchParams.set("sortBy", sortBy);
@@ -49,12 +32,11 @@ document.addEventListener("DOMContentLoaded", function () {
                     url.searchParams.delete("sortBy");
                     url.searchParams.delete("sortOrder");
                 }
-                url.searchParams.set("page", "1"); // Reset to first page
+                url.searchParams.set("page", "1");
                 window.location.href = url.toString();
             });
         });
 
-        // Close dropdown when clicking outside
         $(document).on("click", function (e) {
             const clickedElement = $(e.target);
             const isClickInside =
