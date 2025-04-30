2eff81cc3b3f0a61851d2d5d3841e04702d63e90
vs <valeriisklyarov@gmail.com>
2025-04-30 16:58:59 +0300

    feat(services): add reset route to ServicesController

--- routes/services.php
+++ routes/services.php
@@ -9,4 +9,5 @@ Route::middleware('web')->group(function () {
     Route::get('/services/{id}', [ServicesController::class, 'show'])->name('services.show');
     Route::post('/services/{id}/rate', [ServicesController::class, 'rate'])->name('services.rate');
     Route::get('/services/redirect/{service}', [ServiceRedirectController::class, 'redirect'])->name('services.redirect');
+    Route::get('/services/reset', [ServicesController::class, 'reset'])->name('services.reset');
 });
