83000b7eeda5525c0a25721b101d7e05f3fcf064
vs <valeriisklyarov@gmail.com>
2025-05-02 17:35:30 +0300

    refactor(blog-mobile-filter): replace static categories with dynamic rendering

--- resources/views/partials/blog-mobile-filter.blade.php
+++ resources/views/partials/blog-mobile-filter.blade.php
@@ -8,15 +8,20 @@
     </div>
     <div class="filter__content _blog">
         <ul class="blog-nav pb-3">
-            <li><a href="#"><span>Push traffic</span> <span class="blog-nav__count">12</span></a></li>
-            <li class="is-active"><a href="#"><span>Teaser networks</span> <span class="blog-nav__count">66</span></a></li>
-            <li><a href="#"><span>CPA networks</span> <span class="blog-nav__count">23</span></a></li>
-            <li><a href="#"><span>Social networks</span> <span class="blog-nav__count">56</span></a></li>
-            <li><a href="#"><span>SMM</span> <span class="blog-nav__count">38</span></a></li>
-            <li><a href="#"><span>Events</span> <span class="blog-nav__count">98</span></a></li>
-            <li><a href="#"><span>Lead generation</span> <span class="blog-nav__count">11</span></a></li>
-            <li><a href="#"><span>News</span> <span class="blog-nav__count">2</span></a></li>
-            <li><a href="#"><span>Service reviews</span> <span class="blog-nav__count">4</span></a></li>
+            @php
+            $activeCategory = $currentCategory ? $currentCategory : null;
+            $activeCategoryId = $activeCategory ? $activeCategory->id : null;
+            @endphp
+            @foreach($categories['categories'] as $category)
+            <li 
+                class="{{ $category->id == $activeCategoryId ? 'is-active' : '' }}">
+                <a href="{{ route('blog.category', $category->slug) }}">
+                    <span>{{ $category->name }}</span> 
+                    <span class="blog-nav__count">{{ $category->posts_count }}</span>
+                </a>
+            </li>
+            @endforeach
+           
         </ul>
     </div>
 </div>
