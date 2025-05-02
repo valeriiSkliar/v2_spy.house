5bad8c574fce191657315ff5c8c12e05a716439a
vs <valeriisklyarov@gmail.com>
2025-05-02 09:16:54 +0300

    Add empty state component when no landings are available

--- resources/views/components/landings/table.blade.php
+++ resources/views/components/landings/table.blade.php
@@ -1,5 +1,6 @@
 <div class="c-table">
     <div class="inner">
+        @if($landings->count() > 0)
         <table class="table no-wrap-table">
             <x-landings.table.head
                 :headers="[ 'landings.table.header.downloadLink', 'landings.table.header.dateAdded', '']" />
@@ -9,6 +10,9 @@
                 @endforeach
             </tbody>
         </table>
+        @else
+        <x-empty-landings />
+        @endif
     </div>
 </div>
 
