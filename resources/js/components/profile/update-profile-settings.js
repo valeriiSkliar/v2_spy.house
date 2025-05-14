ab3a89c80c9482b0d72f2d3e6919df2ff7c25286
vs <valeriisklyarov@gmail.com>
2025-05-14 14:49:16 +0300

    feat(profile): implement profile settings update form submission logic

--- resources/js/components/profile/update-profile-settings.js
+++ resources/js/components/profile/update-profile-settings.js
@@ -12,3 +12,39 @@ export const updateProfileSettingsHandler = {
         console.log("Update profile settings handler initialized");
     },
 };
+
+const updateProfileSettings = () => {
+    const form = $("#personal-settings-form");
+    if (form.length) {
+        form.on("submit", async function (e) {
+            try {
+                e.preventDefault();
+                console.log("Submit event triggered");
+                const formData = new FormData(this);
+                loader.show();
+                const response = await ajaxFetcher.form(
+                    config.apiProfileSettingsEndpoint,
+                    formData
+                );
+                if (response.success) {
+                    createAndShowToast(response.message, "success");
+                } else {
+                    createAndShowToast(response.message, "error");
+                }
+            } catch (error) {
+                console.error("Error updating profile settings:", error);
+                createAndShowToast("Error updating profile settings", "error");
+            } finally {
+                loader.hide();
+            }
+        });
+    }
+};
+
+const initUpdateProfileSettings = () => {
+    if ($("#personal-settings-form").length) {
+        updateProfileSettings();
+    }
+};
+
+export { initUpdateProfileSettings };
