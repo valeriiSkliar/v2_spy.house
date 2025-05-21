93f5a84482df90a0adee59e15629f21bf111adf0
vs <valeriisklyarov@gmail.com>
2025-05-21 17:35:01 +0300

    fix(pagination): add data attributes for page navigation links

--- resources/views/common/pagination/spy-pagination-default.blade.php
+++ resources/views/common/pagination/spy-pagination-default.blade.php
@@ -3,37 +3,47 @@
     <ul class="pagination-list">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <li><a class="pagination-link prev disabled" aria-disabled="true" href=""><span class="icon-prev"></span> <span class="pagination-link__txt">Previous</span></a></li>
+        <li><a data-page="prev" class="pagination-link prev disabled" aria-disabled="true" href=""><span
+                    class="icon-prev"></span> <span class="pagination-link__txt">Previous</span></a></li>
         @else
-            <li><a class="pagination-link prev" aria-disabled="false" href="{{ $paginator->previousPageUrl() }}"><span class="icon-prev"></span> <span class="pagination-link__txt">Previous</span></a></li>
+        <li><a data-page="prev" class="pagination-link prev" aria-disabled="false"
+                href="{{ $paginator->previousPageUrl() }}"><span class="icon-prev"></span> <span
+                    class="pagination-link__txt">Previous</span></a></li>
         @endif
 
         {{-- Pagination Elements --}}
         @foreach ($elements as $element)
-            {{-- "Three Dots" Separator --}}
-            @if (is_string($element)) {{-- This is for "..." separator which is not in your new template design directly but Laravel's default logic might produce it. Let's assume for now the new template doesn't want "..." and we'd rely only on numbered links and prev/next. If "..." is needed, the template needs a <li> for it. The provided loop for $elements usually handles this. The provided template seems to only iterate if $element is an array for numbered pages. We'll keep Laravel's default $elements loop as it's robust. --}}
-                @if (is_string($element))
-                    <li class="disabled"><span>{{ $element }}</span></li> {{-- Standard way to show "..." --}}
-                @endif
-            @endif
+        {{-- "Three Dots" Separator --}}
+        @if (is_string($element)) {{-- This is for "..." separator which is not in your new template design directly but
+        Laravel's default logic might produce it. Let's assume for now the new template doesn't want "..." and we'd rely
+        only on numbered links and prev/next. If "..." is needed, the template needs a <li> for it. The provided loop
+            for $elements usually handles this. The provided template seems to only iterate if $element is an array for
+            numbered pages. We'll keep Laravel's default $elements loop as it's robust. --}}
+            @if (is_string($element))
+        <li class="disabled"><span>{{ $element }}</span></li> {{-- Standard way to show "..." --}}
+        @endif
+        @endif
 
-            {{-- Array Of Links --}}
-            @if (is_array($element))
-                @foreach ($element as $page => $url)
-                    @if ($page == $paginator->currentPage())
-                        <li><a class="pagination-link active" href="#" aria-current="page">{{ $page }}</a></li>
-                    @else
-                        <li><a class="pagination-link" href="{{ $url }}">{{ $page }}</a></li>
-                    @endif
-                @endforeach
-            @endif
+        {{-- Array Of Links --}}
+        @if (is_array($element))
+        @foreach ($element as $page => $url)
+        @if ($page == $paginator->currentPage())
+        <li><a data-page="{{ $page }}" class="pagination-link active" href="#" aria-current="page">{{ $page }}</a></li>
+        @else
+        <li><a data-page="{{ $page }}" class="pagination-link" href="{{ $url }}">{{ $page }}</a></li>
+        @endif
+        @endforeach
+        @endif
         @endforeach
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <li><a class="pagination-link next" aria-disabled="false" href="{{ $paginator->nextPageUrl() }}"><span class="pagination-link__txt">Next</span> <span class="icon-next"></span></a></li>
+        <li><a data-page="next" class="pagination-link next" aria-disabled="false"
+                href="{{ $paginator->nextPageUrl() }}"><span class="pagination-link__txt">Next</span> <span
+                    class="icon-next"></span></a></li>
         @else
-            <li><a class="pagination-link next disabled" aria-disabled="true" href=""><span class="pagination-link__txt">Next</span> <span class="icon-next"></span></a></li>
+        <li><a data-page="next" class="pagination-link next disabled" aria-disabled="true" href=""><span
+                    class="pagination-link__txt">Next</span> <span class="icon-next"></span></a></li>
         @endif
     </ul>
     @endif
