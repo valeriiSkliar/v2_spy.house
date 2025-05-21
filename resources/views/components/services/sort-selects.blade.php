12e77f35e2f244a7d82a4ebdd44d215e539cd486
vs <valeriisklyarov@gmail.com>
2025-05-21 16:10:04 +0300

    refactor(services): simplify sort-selects component structure

--- resources/views/components/services/sort-selects.blade.php
+++ resources/views/components/services/sort-selects.blade.php
@@ -1,44 +1,24 @@
 @props([
-    'sortOptions' => [],
-    'perPageOptions' => [],
-    'selectedSort' => ['value' => '', 'order' => '', 'label' => ''],
-    'selectedPerPage' => ['value' => '', 'order' => '', 'label' => ''],
-    'filters' => [],
-    'sortOptionsPlaceholder' => '',
-    'perPageOptionsPlaceholder' => '',
+'sortOptions' => [],
+'perPageOptions' => [],
+'selectedSort' => ['value' => '', 'order' => '', 'label' => ''],
+'selectedPerPage' => ['value' => '', 'order' => '', 'label' => ''],
+'filters' => [],
+'sortOptionsPlaceholder' => '',
+'perPageOptionsPlaceholder' => '',
 ])
 
 <div class="col-12 col-md-6 col-lg-auto mb-15">
     <div class="base-select-icon">
-        <x-common.base-select
-                id="sort-by"
-                {{-- :selected="[
-                    'value' => $filters['sortBy'] ?? 'default',
-                    'order' => $filters['sortOrder'] ?? 'asc',
-                    'label' => 'Sort by — ' . ($filters['sortBy'] ?? 'Transitions High to Low')
-                ]" --}}
-                :selected="$selectedSort"
-                :options="$sortOptions" 
-                :placeholder="$sortOptionsPlaceholder"
-                :icon="'sort'"
-            />
+        <x-common.base-select id="sort-by" :selected="$selectedSort" :options="$sortOptions"
+            :placeholder="$sortOptionsPlaceholder" :icon="'sort'" />
         <span class="icon-sort"></span>
     </div>
 </div>
 <div class="col-12 col-md-6 col-lg-auto mb-15">
     <div class="base-select-icon">
-        <x-common.base-select
-            id="per-page"
-            {{-- :selected="[
-                'value' => $filters['perPage'] ?? '12',
-                'order' => '',
-                'label' => 'On page — ' . ($filters['perPage'] ?? '12')
-            ]" --}}
-            :selected="$selectedPerPage"
-            :options="$perPageOptions" 
-            :placeholder="$perPageOptionsPlaceholder"
-            :icon="'list'"
-        />
+        <x-common.base-select id="per-page" :selected="$selectedPerPage" :options="$perPageOptions"
+            :placeholder="$perPageOptionsPlaceholder" :icon="'list'" />
         <span class="icon-list"></span>
     </div>
 </div>
