e0640c85a45022c4f2c3029c9222c002405a5ef4
vs <valeriisklyarov@gmail.com>
2025-05-26 17:56:20 +0300

    fix(auth/password-reset): change view for password recovery page

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -16,7 +16,7 @@ class PasswordResetLinkController extends Controller
      */
     public function create(): View
     {
-        return view('auth.forgot-password');
+        return view('pages.password_recovery');
     }
 
     /**
