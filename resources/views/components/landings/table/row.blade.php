1228dd3b61a2730663e5a0bb1ef65dfcdb401317
vs <valeriisklyarov@gmail.com>
2025-05-28 15:07:45 +0300

    fix(table): update landing creation date format to include time

--- resources/views/components/landings/table/row.blade.php
+++ resources/views/components/landings/table/row.blade.php
@@ -1,8 +1,9 @@
 @props(['landing' => []])
 <tr>
     <td><span>{{ $landing['id'] }}</span></td>
-    <td><span class="table-date">{{ Carbon\Carbon::parse($landing['created_at'])->format('d.m.Y' ) }}</span></td>
-    <td><a target="_blank" href="{{ $landing['url'] }}" class="table-link icon-link-arrow"><span>{{ $landing['url'] }}</span></a></td>
+    <td><span class="table-date">{{ Carbon\Carbon::parse($landing['created_at'])->format('d.m.Y / H:i' ) }}</span></td>
+    <td><a target="_blank" href="{{ $landing['url'] }}" class="table-link icon-link-arrow"><span>{{ $landing['url']
+                }}</span></a></td>
     <td>
         <ul class="table-controls justify-content-end">
             @if($landing['status'] !== 'completed')
@@ -12,13 +13,13 @@
             @endif
             @if($landing['status'] === 'completed')
             <li>
-                <button type="button" class="btn-icon icon-download download-landing-button" data-id="{{ $landing->id }}"></button>
+                <button type="button" class="btn-icon icon-download download-landing-button"
+                    data-id="{{ $landing->id }}"></button>
             </li>
             @endif
             <li>
                 <button @if($landing['status']==='pending' ) disabled @endif type="button"
-                    class="btn-icon icon-remove delete-landing-button"
-                    data-id="{{ $landing->id }}"
+                    class="btn-icon icon-remove delete-landing-button" data-id="{{ $landing->id }}"
                     data-confirm="{{ __('landings.table.confirmDelete.message') }}"
                     data-confirm-title="{{ __('landings.table.confirmDelete.title') }}"
                     data-confirm-btn="{{ __('landings.table.confirmDelete.confirmButton') }}"
