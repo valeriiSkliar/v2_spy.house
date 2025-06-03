542958b16891a8a4a639b2330a5d3dfd5c656a54
vs <valeriisklyarov@gmail.com>
2025-06-03 11:04:03 +0300

    refactor(notifications): change getNotifications to protected and add AJAX list method

---
+++ resources/views/components/notifications/empty-notifications.blade.php
@@ -0,0 +1,7 @@
+<div class="notification-empty text-center py-5">
+    <div class="icon-wrapper mb-4">
+        <i class="icon-bell" style="font-size: 48px; color: #ccc;"></i>
+    </div>
+    <h3>{{ __('notifications.empty.title') }}</h3>
+    <p class="text-muted">{{ __('notifications.empty.description') }}</p>
+</div>
---
+++ resources/views/components/notifications/notifications-list.blade.php
@@ -0,0 +1,11 @@
+<div class="notification-list">
+    @foreach($notifications as $notification)
+    <x-notification-item
+        :id="$notification['id']"
+        :read="$notification['read']"
+        :date="$notification['date']"
+        :title="$notification['title']"
+        :content="$notification['content']"
+        :hasButton="$notification['hasButton'] ?? false" />
+    @endforeach
+</div>
