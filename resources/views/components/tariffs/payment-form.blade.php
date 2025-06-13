41f6063a78ec94fdfb3dc1b750370fe4cdb3a37f
vs <valeriisklyarov@gmail.com>
2025-06-13 11:27:12 +0300

    feat(tariffs/payment-form): promo code input display only for monthly billing

--- resources/views/components/tariffs/payment-form.blade.php
+++ resources/views/components/tariffs/payment-form.blade.php
@@ -22,10 +22,12 @@
             <input type="hidden" name="payment_method" id="selected_payment_method"
                 value="{{  App\Enums\Finance\PaymentMethod::USDT }}">
 
+            @if($billingType === 'month')
             <div class="form-item mb-25">
                 <label class="d-block mb-10 font-weight-600">Promo Code</label>
                 <input type="text" name="promo_code" class="input-h-57" value="">
             </div>
+            @endif
             <div class="mb-20">
                 <button type="submit" class="btn _flex _green _big min-200 w-mob-100">Proceed to payment</button>
             </div>
