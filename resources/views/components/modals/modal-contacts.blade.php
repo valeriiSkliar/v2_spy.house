1f7b2ae8780c0a37a9c745104fa02f17a928d0be
vs <valeriisklyarov@gmail.com>
2025-06-13 12:52:28 +0300

    feat(modals): add contacts and subscription activated modals

---
+++ resources/views/components/modals/modal-contacts.blade.php
@@ -0,0 +1,57 @@
+<div class="modal fade modal-contacts" id="modal-contacts" style="z-index: 10005;">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <button type="button" class="btn-icon _gray btn-close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true"><span class="icon-x"></span></span>
+            </button>
+            <div class="modal-head">
+                <h2 class="mb-2">Сontacts</h2>
+                <p>If you have any questions, you can write to any of our managers</p>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-6 mb-10">
+                    <a href="#" target="_blank" class="manager">
+                        <span class="icon-telegram"></span>
+                        <span class="manager__thumb"><img src="img/manager-1.png" alt=""></span>
+                        <span class="manager__content">
+                            <span class="manager__name">Maksim</span>
+                            <span class="manager__link">@Max_spy_house</span>
+                        </span>
+                    </a>
+                </div>
+                <div class="col-12 col-md-6 mb-10">
+                    <a href="#" target="_blank" class="manager">
+                        <span class="icon-telegram"></span>
+                        <span class="manager__thumb"><img src="img/manager-2.svg" alt=""></span>
+                        <span class="manager__content">
+                            <span class="manager__name">Telegram chat</span>
+                            <span class="manager__link">@spy_house_chat</span>
+                        </span>
+                    </a>
+                </div>
+            </div>
+            <div class="sep"></div>
+            <h3 class="mb-2">Or use the form below</h3>
+            <p class="mb-20">If you have any suggestions or wishes, please write to us.</p>
+            <form action="">
+                <div class="row _offset20">
+                    <div class="col-12 col-md-6 mb-15">
+                        <input type="text" placeholder="Name">
+                    </div>
+                    <div class="col-12 col-md-6 mb-15">
+                        <input type="email" placeholder="G-mail">
+                    </div>
+                    <div class="col-12 mb-15">
+                        <textarea placeholder="Message"></textarea>
+                    </div>
+                    <div class="col-6 mb-15">
+                        <img src="img/reCAPTCHA%20v2%20checkbox.png" alt="" class="w-100">
+                    </div>
+                    <div class="col-12 mb-15">
+                        <button type="submit" class="btn _flex _green _medium min-120 w-mob-100">Send</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
---
+++ resources/views/components/modals/subscribtion-activated.blade.php
@@ -0,0 +1,27 @@
+<div class="modal fade modal-subscription-activated" id="modal-subscription-activated" style="z-index: 10005;">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <button type="button" class="btn-icon _gray btn-close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true"><span class="icon-x"></span></span>
+            </button>
+            <div class="subscription-activated">
+                <div class="subscription-activated-icon icon-check-circle">
+                    <div class="tariff-name _start">Start</div>
+                </div>
+                <h2 class="font-20 mb-20">Subscription activated</h2>
+                <p class="mb-30">
+                    Your <strong>"Start"</strong> subscription is active. <br>
+                    Valid until: <span class="icon-clock"></span> <strong>02.05.25</strong>
+                </p>
+                <div class="row justify-content-center">
+                    <div class="col-auto">
+                        <button class="btn _flex _green _medium min-120">Ok</button>
+                    </div>
+                    <div class="col-auto">
+                        <button class="btn _flex _gray _medium min-120">Change tariff</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
