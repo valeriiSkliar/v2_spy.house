12f480abd3623f199ed0c2040d1c517a285e7b1a
vs <valeriisklyarov@gmail.com>
2025-06-24 19:49:29 +0300

    refactor(bootstrap): comment out unused swiper import

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 // Libs
 import 'bootstrap-select';
 import 'css-element-queries';
-import 'swiper';
+// import 'swiper';
 
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
