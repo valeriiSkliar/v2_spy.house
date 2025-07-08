6c7049c9126def549daa5cffba8cb0b42d327e4a
vs <valeriisklyarov@gmail.com>
2025-07-08 15:13:02 +0300

    feat(layouts): create new body layout and update main page structure

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,29 +1,8 @@
-@php
-use Illuminate\Support\Facades\Auth;
-@endphp
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.body')
+@section('content')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
-    @if(Auth::check() && isset($api_token))
-    <meta name="api-token" content="{{ $api_token }}">
-    @if(isset($api_token_expires_at))
-    <meta name="api-token-expires-at" content="{{ $api_token_expires_at }}">
-    @endif
-    @endif
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    @vite(['resources/js/app.js', 'resources/scss/app.scss'])
-
-    <!-- Frontend Translations -->
-    <x-frontend-translations />
-</head>
+<!-- Scripts -->
+@vite(['resources/js/app.js', 'resources/scss/app.scss'])
 
 <body class="">
     <div class="navigation-bg"></div>
@@ -84,6 +63,4 @@ use Illuminate\Support\Facades\Auth;
     <!-- Routes -->
     @stack('scripts')
     @stack('modals')
-</body>
-
-</html>
+    @endsection
