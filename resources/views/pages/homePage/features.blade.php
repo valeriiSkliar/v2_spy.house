b3c13ef3d56e66c5ba30865af41c293fcf541c1d
vs <valeriisklyarov@gmail.com>
2025-07-11 18:04:08 +0300

    feat(frontend/home): add features block with localization support

--- resources/views/pages/homePage/features.blade.php
+++ resources/views/pages/homePage/features.blade.php
@@ -2,40 +2,38 @@
     <div class="container">
         <div class="row align-items-end _offset30">
             <div class="col-12 col-md-6">
-                <div class="title-label" data-aos-delay="200" data-aos="fade-up">Features</div>
-                <h2 class="title" data-aos-delay="200" data-aos="fade-up">Find effective ads</h2>
+                <div class="title-label" data-aos-delay="200" data-aos="fade-up">{{ __('main_page.features_blok.label')
+                    }}</div>
+                <h2 class="title" data-aos-delay="200" data-aos="fade-up">{{ __('main_page.features_blok.title') }}</h2>
             </div>
             <div class="col-12 col-md-6 pb-2">
-                <div class="section-desc icon-txt-dot mb-30" data-aos-delay="200" data-aos="fade-up">If you want to
-                    save time - Use Spy.house analytics and launch profitable campaigns based on the successful
-                    experience of competitors</div>
+                <div class="section-desc icon-txt-dot mb-30" data-aos-delay="200" data-aos="fade-up">{{
+                    __('main_page.features_blok.description') }}</div>
             </div>
         </div>
         <div class="features__list row _offset20">
             <div class="col-12 col-lg-7 d-flex" data-aos-delay="200" data-aos="fade-up">
                 <div class="feature-item _bg">
-                    <h3>185+ countries of the world</h3>
-                    <p>We collect creatives from almost all over the world and constantly increase this number. Rest
-                        assured, no one will go unnoticed</p>
+                    <h3>{{ __('main_page.features_blok.countries_title') }}</h3>
+                    <p>{{ __('main_page.features_blok.countries_desc') }}</p>
                 </div>
             </div>
             <div class="col-12 col-md-7 col-lg-5 d-flex" data-aos-delay="200" data-aos="fade-up">
                 <div class="feature-item">
-                    <h3>12M daily creatives</h3>
-                    <p>All your competitors' creatives can be found with us and save money and time</p>
+                    <h3>{{ __('main_page.features_blok.daily_creatives_title') }}</h3>
+                    <p>{{ __('main_page.features_blok.daily_creatives_desc') }}</p>
                 </div>
             </div>
             <div class="col-12 col-md-5 col-lg-4 d-flex" data-aos-delay="200" data-aos="fade-up">
                 <div class="feature-item">
-                    <h3>100% positive</h3>
-                    <p>All users are satisfied with our service</p>
+                    <h3>{{ __('main_page.features_blok.positive_title') }}</h3>
+                    <p>{{ __('main_page.features_blok.positive_desc') }}</p>
                 </div>
             </div>
             <div class="col-12 col-lg-8 d-flex" data-aos-delay="200" data-aos="fade-up">
                 <div class="feature-item">
-                    <h3>16+ different filters to find the right ads</h3>
-                    <p>Advanced search and filtering settings allow you to find exactly the creatives you need for
-                        effective work</p>
+                    <h3>{{ __('main_page.features_blok.filters_title') }}</h3>
+                    <p>{{ __('main_page.features_blok.filters_desc') }}</p>
                 </div>
             </div>
         </div>
