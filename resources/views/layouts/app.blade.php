16a7e443fbf2cfd8dd28a727a13dcbca0393c78d
vs <valeriisklyarov@gmail.com>
2025-07-15 15:18:41 +0300

    feat(lang/profile): add English translations for profile settings

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,29 @@
-@extends('layouts.body')
-@section('content')
+@php
+use Illuminate\Support\Facades\Auth;
+@endphp
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<!-- Scripts -->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+    @if(Auth::check() && isset($api_token))
+    <meta name="api-token" content="{{ $api_token }}">
+    @if(isset($api_token_expires_at))
+    <meta name="api-token-expires-at" content="{{ $api_token_expires_at }}">
+    @endif
+    @endif
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    @vite(['resources/js/app.js', 'resources/scss/app.scss'])
+
+    <!-- Frontend Translations -->
+    <x-frontend-translations />
+</head>
 
 <body class="">
     <div class="navigation-bg"></div>
@@ -49,8 +71,6 @@
     </script>
     @endif
 
-    <!-- Routes for ajax -->
-    @if (Auth::check())
     <script>
         window.routes = {
             landingsAjaxList: '{{ route("landings.list.ajax") }}',
@@ -58,8 +78,8 @@
             landingsAjaxDestroyBase: '{{ route("landings.destroy.ajax", ["landing" => ":id"]) }}',
         };
     </script>
-    @endif
-    <!-- Routes -->
     @stack('scripts')
     @stack('modals')
-    @endsection
+</body>
+
+</html>
