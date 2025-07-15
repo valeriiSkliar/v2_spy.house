7a1f838cdc24f2d4940c61261aeffec7f2cbac0e
vs <valeriisklyarov@gmail.com>
2025-07-15 19:27:09 +0300

    [feat(lang/common): add English error and success messages]

---
+++ lang/en/common.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'error' => [
+        'invalid_language_selected' => 'Invalid language selected',
+        'invalid_credentials' => 'Operation not available.',
+        'user_not_found_for_refresh_token' => 'Operation not available.',
+        'refresh_token_not_provided' => 'Operation not available.',
+        'refresh_token_invalid_or_expired' => 'Operation not available.',
+        'refresh_token_not_provided_and_user_not_authenticated' => 'Operation not available.',
+        'token_not_found' => 'Operation not available.',
+    ],
+    'success' => [
+        'logged_out' => 'Operation completed successfully.',
+        'new_token_created' => 'Operation completed successfully.',
+        'current_token_revoked_successfully' => 'Operation completed successfully.',
+    ],
+    'copy' => [
+        'copy' => 'Copy',
+        'copied' => 'Copied',
+    ],
+];
