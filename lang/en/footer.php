2c5a69c1fa9d9c68a33b3202cc406eb4f84b1347
vs <valeriisklyarov@gmail.com>
2025-07-15 19:40:52 +0300

    feat(lang/en/footer): add English translations for footer content

---
+++ lang/en/footer.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'copyright' => '© 2012 - :year Spy.House',
+    'creatives_spy_title' => 'Creative Search',
+    'push' => 'Push',
+    'in_page' => 'In-page',
+    'facebook' => 'Facebook',
+    'tiktok' => 'Tik Tok',
+    'useful_title' => 'Useful',
+    'offers' => 'Offers',
+    'services' => 'Services',
+    'affiliate_program' => 'Affiliate Program',
+    'blog' => 'Blog',
+    'other_title' => 'Other',
+    'advertising' => 'Advertising',
+    'privacy_policy' => 'Privacy Policy',
+    'user_agreement' => 'User Agreement',
+    'faq' => 'FAQ',
+    'telegram_label' => 'Telegram',
+    'telegram_nickname' => '@spyhouse_help',
+    'telegram_chat_button' => 'Telegram Chat',
+];
