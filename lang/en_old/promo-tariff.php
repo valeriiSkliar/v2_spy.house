b5d7d0bd389ae48151641f4266f85306bb8ccbe0
vs <valeriisklyarov@gmail.com>
2025-07-15 12:49:55 +0300

    [refactor(lang): remove old language files for cleanup

--- lang/en_old/promo-tariff.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'upgrade_title' => 'Upgrade to Premium',
-    'upgrade_desc' => '<span>-50%</span> In the first month',
-    'go_button' => 'Go',
-];
--- lang/en_old/sidebar.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'free_tariff' => 'Free',
-    'copyright' => '© 2012 - 2025 Spy.House',
-];
