94347a2532e05fdc1332f91a18306bb06990742c
vs <valeriisklyarov@gmail.com>
2025-07-15 19:32:45 +0300

    feat(lang/en/main_page): add English translations for main page content

---
+++ lang/en/main_page.php
@@ -0,0 +1,76 @@
+<?php
+
+return [
+    'footer' => [
+        'copyright' => '© 2012 - :year Spy.House',
+        'creatives_spy_title' => 'Creatives Spy',
+        'push' => 'Push',
+        'inpage' => 'In-page',
+        'facebook' => 'Facebook',
+        'tiktok' => 'Tik Tok',
+        'useful_title' => 'Useful',
+        'offers' => 'Offers',
+        'services' => 'Services',
+        'affiliate_program' => 'Affiliate Program',
+        'blog' => 'Blog',
+        'other_title' => 'Other',
+        'advertising' => 'Advertising',
+        'privacy_policy' => 'Privacy Policy',
+        'user_agreement' => 'User Agreement',
+        'faq' => 'FAQ',
+        'telegram_label' => 'Telegram',
+        'telegram_nickname' => '@spyhouse_help',
+        'telegram_chat' => 'Telegram chat',
+    ],
+    'offer' => [
+        'title' => 'All competitor creatives are here',
+        'description' => 'Spy House — a service for spying on advertising in popular formats',
+        'winner_title' => 'Best Affiliate Spy Tool',
+        'winner_desc' => 'Conversion Club 2021 Award',
+    ],
+    'features_blok' => [
+        'label' => 'Features',
+        'title' => 'Find effective advertising',
+        'description' => 'If you want to save time - use Spy.house analytics and launch profitable campaigns based on competitors\' successful experience',
+        'countries_title' => '185+ countries worldwide',
+        'countries_desc' => 'We collect creatives from almost all over the world and constantly increase this number. Be sure, no one will go unnoticed',
+        'daily_creatives_title' => '12M creatives daily',
+        'daily_creatives_desc' => 'All your competitors\' creatives can be found with us and save money and time',
+        'positive_title' => '100% positive',
+        'positive_desc' => 'All users are satisfied with our service',
+        'filters_title' => '16+ different filters to find the right advertising',
+        'filters_desc' => 'Advanced search and filtering settings allow you to find exactly those creatives that are needed for effective work',
+    ],
+    'creatives_blok' => [
+        'title' => 'Creatives from all popular verticals',
+        'subtitle' => 'You will definitely find creatives in your vertical'
+    ],
+    'blog_blok' => [
+        'title' => 'New cases in the blog',
+        'go_to_blog' => 'Go to blog',
+    ],
+    'reviews_blok' => [
+        'title' => 'Reviews',
+        'about_us' => 'What media buyers say about us',
+        'add_review' => 'Add review',
+    ],
+    'download_creatives_blok' => [
+        'title' => 'Download competitor creatives <br>with 25% discount',
+        'button' => 'Download',
+    ],
+    'prices_blok' => [
+        'title' => 'Big investments <br>for small money',
+        'description' => 'Expand your capabilities with Pro plan with annual subscription',
+        'button' => 'Per month',
+        'button_year' => 'Per year',
+        'button_year_discount' => 'For a year :discount%',
+    ],
+
+    'features' => 'Features',
+    'prices' => 'Prices',
+    'reviews' => 'Reviews',
+    'blog' => 'Blog',
+    'contacts' => 'Contacts',
+    'get_started' => 'Get Started',
+
+];
