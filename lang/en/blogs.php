8bbac2eb52a7a7e0b1116ecdebb1a106177b7ef5
vs <valeriisklyarov@gmail.com>
2025-07-15 13:12:27 +0300

    [feat(lang): add English blog language file

---
+++ lang/en/blogs.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+    'breadcrumbs' => [
+        'blog' => 'Blog',
+        'search_results' => 'Search results for ":query"',
+    ],
+    'header' => [
+        'title' => 'Blog',
+        'back_to_ads' => 'Back to ads',
+        'back_to_blogs' => 'Back to blogs',
+    ],
+    'all_categories' => 'All categories',
+    'new_article_label' => 'New',
+    'read_often' => 'Frequently read',
+    'it_will_also_be_interesting' => 'You might also be interested',
+    'no_results' => [
+        'title' => 'No results found for ":query"',
+        'description' => 'Try other keywords or check out our categories below.',
+    ],
+    'comments' => [
+        'title' => 'Comments',
+        'leave_comment' => 'To leave a comment',
+        'login' => 'Login',
+        'to_service' => 'to your Spy.house account',
+        'leave_reply' => 'Leave a reply',
+        'reply' => 'Reply',
+        'cancel' => 'Cancel',
+        'send' => 'Send',
+        'leave_reply_in_response_to' => 'Leave a reply to the comment',
+        'write_comment' => 'Write your comment text',
+        'comment_added_successfully' => 'Comment submitted successfully. It will appear on the site after moderator approval.',
+        'flood_protection_message' => 'Operation is temporarily unavailable. Please try again later.',
+        'content_required' => 'Please enter comment text.',
+        'content_min' => 'The comment must be at least :min characters long.',
+        'content_max' => 'The comment must not exceed :max characters.',
+    ],
+    'article_rating' => [
+        'leave_rating' => 'To leave a rating, please',
+        'login' => 'login',
+        'to_service' => 'to your Spy.house account',
+        'rate_this_article' => 'Rate this article',
+        'rate_from_1_to_5' => 'Rate from 1 to 5',
+        'thank_you_for_rating' => 'Thank you for your rating!',
+        'you_rated_this_article' => 'Operation completed.',
+        'stars' => 'star(s)',
+        'average_rating' => 'Average rating',
+    ],
+];
