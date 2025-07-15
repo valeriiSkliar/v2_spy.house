bf8cf464ef01a88e274d67fdeb4a8a334c4102c0
vs <valeriisklyarov@gmail.com>
2025-07-15 13:00:22 +0300

    [feat(lang): add English blog language file

---
+++ lang/en/blog.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'category' => 'Category',
+    'search_by_keyword' => 'Search by keyword',
+    'view_all_results' => 'View all :total results',
+    'success' => [
+        'search_results_fetched_successfully' => 'Search results fetched successfully',
+    ],
+    'messages' => [
+        'comment_submitted_successfully' => 'Comment submitted successfully and is awaiting moderation.',
+        'comment_approved_successfully' => 'Comment approved successfully.',
+        'comment_deleted_successfully' => 'Comment deleted successfully.',
+        'rating_submitted_successfully' => 'Rating submitted successfully.',
+    ],
+    'errors' => [
+        'invalid_parent_comment' => 'Operation not available.',
+        'no_comments_found' => 'No comments found',
+        'you_must_be_logged_in_to_rate_articles' => 'Operation not available.',
+        'you_have_already_rated_this_article' => 'Operation not available.',
+        'cannot_reply_to_own_comment' => 'Operation not available.',
+        'you_must_be_logged_in_to_submit_a_comment' => 'Operation not available. You must be logged in to submit a comment.',
+    ],
+];
