e2b6d07d83e4b5693794d231f85907f459c3af4f
vs <valeriisklyarov@gmail.com>
2025-07-19 15:20:00 +0300

    refactor(services): update layout structure and improve responsiveness

--- resources/views/layouts/body.blade.php
+++ resources/views/layouts/body.blade.php
@@ -47,8 +47,12 @@ use Illuminate\Support\Facades\Auth;
     <script src="{{ asset('js/typeit.min.js') }}"></script>
     <script src="{{ asset('js/mqscroller.min.js') }}"></script>
     <script src="{{ asset('js/aos.js') }}"></script>
+    @if(Request::is('/'))
     <script src="{{ asset('js/main.js') }}"></script>
     <script src="{{ asset('js/home.js') }}"></script>
+    @else
+    @vite(['resources/js/app.js'])
+    @endif
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
