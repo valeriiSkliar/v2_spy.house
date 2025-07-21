8b44ebc8062331f8944fe7547a089e139d4d2160
vs <valeriisklyarov@gmail.com>
2025-07-21 15:04:18 +0300

    refactor(layouts): update title and adjust layout structure

--- resources/views/layouts/main-app.blade.php
+++ resources/views/layouts/main-app.blade.php
@@ -3,15 +3,20 @@
 @section('content')
 <div class="navigation-bg"></div>
 @yield('search-bg')
-<div class="wrapper {{ Request::is('/blog') ? '' : '_page' }}">
+<div
+    class="wrapper {{ Route::is('blog.index') || Route::is('blog.category') || Route::is('blog.show') ? '_page' : '' }}">
     {{-- header --}}
     @include('partials.header')
     {{-- main content --}}
     <main class="main">
-        <div class="{{ Request::is('/blog') ? 'content' : 'container' }}">
+        <div
+            class="{{ Route::is('blog.index') || Route::is('blog.category') || Route::is('blog.show') ? 'container' : 'content' }}">
             @yield('page-content')
         </div>
+        {{-- sidebar --}}
+        @include('partials.sidebar')
     </main>
+
     {{-- fullscreen loader --}}
     <x-common.fullscreen-loader :active="false" />
 </div>
